07fa6bef352931771c68c618b1e8d51b677f3417
kareey-commits <56164343+kareey-commits@users.noreply.github.com>
2020-07-02 17:23:47 +0300

    barbershop appointment booking

---
+++ script.js
@@ -0,0 +1,38 @@
+function divShow() {
+    document.getElementById()
+}
+// // the modal
+// var modal = document.getElementById("myModal");
+
+// // button that opens the modal
+// var btn = document.getElementById("myBtn");
+
+// // span that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // when a user clicks the button open the modal
+// btn.onClick = function() {
+//     modal.style.display = "block";
+// }
+// // when a user clicks on the span,close the modal
+// span.onClick = function() {
+//     modal.style.display = "none";
+// }
+
+// // when a user clicks anywhere outside the modal,close it
+// window.onclick = function(event) {
+//     if(event.target == modal) {
+//         modal.style.display = "none";
+//     } 
+// }
+// var style = "status=no, menub ar=no, toolbar=no scrollbars=no";
+// function popup(link) {
+//     window.open(link, "", style);
+
+// function openForm() {
+//     document.getElementById("form").style.display = "block;"
+// }
+
+// function closeForm() {
+//     document.getElementById("form").style.display = none;
+// }
